3c8ae5bd37be0e3177b826f6b2a1a88bc8f4e295
Renato de Matos Môra <42642436+renatodematosmora@users.noreply.github.com>
2023-12-06 21:22:58 -0300

    Add files via upload

---
+++ FilmesApi/Data/FilmeContext.cs
@@ -0,0 +1,18 @@
+﻿using FilmesApi.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace FilmesApi.Data;
+
+public class FilmeContext : DbContext
+{
+    public FilmeContext(DbContextOptions<FilmeContext> options) : base(options)
+    {
+    }
+
+    public DbSet<Filme> Filmes { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+    }
+}
